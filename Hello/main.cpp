ed4a4a4c3c9015ba7b1fef6379bc99328ebf9569
campbeja <jesslyn.campbell@gmail.com>
2012-09-08 21:52:15 -0500

    Began turn-based LIBTCOD graphic tutorial found at http://roguebasin.roguelikedevelopment.org/index.php?title=Complete_Roguelike_Tutorial,_using_python%2Blibtcod#Start_the_tutorial

--- Hello/main.cpp
+++ Hello/main.cpp
@@ -6,21 +6,21 @@ int main() {
     int height = 50;
 
     TCODConsole::initRoot(width, height, "Libtcod Hello World");
-
-    int x = width / 2;
-    int y = height /2;
     TCOD_key_t key;
-    do
+    int x = width / 2;
+    int y = height / 2;
+
+    while(!TCODConsole::isWindowClosed())
     {
         TCODConsole::root->clear();
-        TCODConsole::root->printCenter(x, y, TCOD_BKGND_NONE, "Libtcod says: Hello, World!");
+        TCODConsole::root->printCenter(x, y, TCOD_BKGND_NONE, "Libtcod says: Hello, World!"); // Breaks when y is same as height
         TCODConsole::flush();
 
-        y++;
+        y = (y >= (height - 1)) ? 0 : y + 1;
 
         key = TCODConsole::waitForKeypress(true);
-    }
-    while(key.c == '\0');
 
-    TCODConsole::waitForKeypress(true);
+        if(key.vk != TCODK_DOWN)
+            return true; // Exit game
+    }
 }
---
+++ Roguelike Tutorial/main.cpp
@@ -0,0 +1,40 @@
+// Code based on tutorial: http://roguebasin.roguelikedevelopment.org/index.php/Complete_Roguelike_Tutorial,_using_python%2Blibtcod,_part_1
+#include "libtcod.hpp"
+
+int main()
+{
+    int SCREEN_WIDTH = 80;
+    int SCREEN_HEIGHT = 50;
+
+    TCODConsole::setCustomFont("arial10x10.png", TCOD_FONT_LAYOUT_TCOD, 0, 0);
+    TCODConsole::initRoot(SCREEN_WIDTH, SCREEN_HEIGHT, "Libtcod Tutorial");
+
+    int playerx = SCREEN_WIDTH / 2;
+    int playery = SCREEN_HEIGHT / 2;
+    TCOD_key_t key;
+    while(!TCODConsole::isWindowClosed())
+    {
+        TCODConsole::root->clear();
+        TCODConsole::root->setForegroundColor(TCODColor::white);
+        TCODConsole::root->printLeft(playerx, playery, TCOD_BKGND_NONE, "@");
+
+        TCODConsole::flush();
+
+        key = TCODConsole::waitForKeypress(true);
+
+        if(key.vk == TCODK_DOWN && playery < SCREEN_HEIGHT - 1)
+            playery += 1;
+        else if(key.vk == TCODK_UP && playery > 0)
+            playery -= 1;
+        else if(key.vk == TCODK_RIGHT && playerx < SCREEN_WIDTH - 1)
+            playerx += 1;
+        else if(key.vk == TCODK_LEFT && playerx > 0)
+            playerx -= 1;
+        else if(key.vk == TCODK_ENTER && key.lalt)
+            TCODConsole::setFullscreen(!TCODConsole::isFullscreen());
+        else if(key.vk == TCODK_ESCAPE)
+            return true;
+    }
+
+    return 0;
+}
