899b41a22a8c9ced00951e62455af44416ff8074
campbeja <jesslyn.campbell@gmail.com>
2012-09-09 21:38:23 -0500

    Refactored key press code into function

--- Roguelike Tutorial/main.cpp
+++ Roguelike Tutorial/main.cpp
@@ -1,17 +1,20 @@
 // Code based on tutorial: http://roguebasin.roguelikedevelopment.org/index.php/Complete_Roguelike_Tutorial,_using_python%2Blibtcod,_part_1
 #include "libtcod.hpp"
 
+int SCREEN_WIDTH = 80;
+int SCREEN_HEIGHT = 50;
+int playerx, playery;
+bool HandleKeys();
+
 int main()
 {
-    int SCREEN_WIDTH = 80;
-    int SCREEN_HEIGHT = 50;
-
     TCODConsole::setCustomFont("arial10x10.png", TCOD_FONT_LAYOUT_TCOD, 0, 0);
     TCODConsole::initRoot(SCREEN_WIDTH, SCREEN_HEIGHT, "Libtcod Tutorial");
 
-    int playerx = SCREEN_WIDTH / 2;
-    int playery = SCREEN_HEIGHT / 2;
-    TCOD_key_t key;
+    playerx = SCREEN_WIDTH / 2;
+    playery = SCREEN_HEIGHT / 2;
+
+    bool exit;
     while(!TCODConsole::isWindowClosed())
     {
         TCODConsole::root->clear();
@@ -20,21 +23,29 @@ int main()
 
         TCODConsole::flush();
 
-        key = TCODConsole::waitForKeypress(true);
-
-        if(key.vk == TCODK_DOWN && playery < SCREEN_HEIGHT - 1)
-            playery += 1;
-        else if(key.vk == TCODK_UP && playery > 0)
-            playery -= 1;
-        else if(key.vk == TCODK_RIGHT && playerx < SCREEN_WIDTH - 1)
-            playerx += 1;
-        else if(key.vk == TCODK_LEFT && playerx > 0)
-            playerx -= 1;
-        else if(key.vk == TCODK_ENTER && key.lalt)
-            TCODConsole::setFullscreen(!TCODConsole::isFullscreen());
-        else if(key.vk == TCODK_ESCAPE)
-            return true;
+        exit = HandleKeys();
+        if(exit)
+            break;
     }
 
     return 0;
+}
+
+bool HandleKeys(){
+    TCOD_key_t key = TCODConsole::waitForKeypress(true);
+
+    if(key.vk == TCODK_DOWN && playery < SCREEN_HEIGHT - 1)
+        playery += 1;
+    else if(key.vk == TCODK_UP && playery > 0)
+        playery -= 1;
+    else if(key.vk == TCODK_RIGHT && playerx < SCREEN_WIDTH - 1)
+        playerx += 1;
+    else if(key.vk == TCODK_LEFT && playerx > 0)
+        playerx -= 1;
+    else if(key.vk == TCODK_ENTER && key.lalt)
+        TCODConsole::setFullscreen(!TCODConsole::isFullscreen());
+    else if(key.vk == TCODK_ESCAPE)
+        return true;
+
+    return false;
 }
