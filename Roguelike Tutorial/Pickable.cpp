cfe6bef9f0da1f6b6feaebdee08810b222ff5f58
campbeja <jesslyn.campbell@gmail.com>
2013-05-12 14:33:57 -0500

    Moved all pickable classes to Pickable .h and .cpp files

--- Roguelike Tutorial/Pickable.cpp
+++ Roguelike Tutorial/Pickable.cpp
@@ -38,3 +38,99 @@ bool Pickable::use(Actor *owner, Actor *wearer)
 
     return false;
 }
+
+Healer::Healer(float amount) : amount(amount)
+{
+}
+
+bool Healer::use(Actor *owner, Actor *wearer)
+{
+    if(wearer->destructible)
+    {
+        float amountHealed = wearer->destructible->heal(amount);
+
+        if(amountHealed > 0)
+        {
+            return Pickable::use(owner, wearer);
+        }
+    }
+
+    return false;
+}
+
+Confuser::Confuser(int nbTurns, float range) : nbTurns(nbTurns), range(range)
+{
+}
+
+bool Confuser::use(Actor *owner, Actor *wearer)
+{
+    engine.gui->message(TCODColor::cyan, "Left-click an enemy to confuse it,\nor right-click to cancel.");
+
+    int x, y;
+    if(!engine.pickATile(&x, &y, range))
+    {
+        return false;
+    }
+
+    Actor *actor = engine.getActor(x, y);
+    if(!actor)
+    {
+        return false;
+    }
+
+    Ai *confuserAi = new ConfusedMonsterAi(nbTurns, actor->ai, actor->col);
+    actor->ai = confuserAi;
+    actor->col = TCODColor::lightBlue;
+
+    engine.gui->message(TCODColor::lightGreen, "The eyes of the %s look vacant,\nas he starts to stumble around!", actor->name);
+
+    return Pickable::use(owner, wearer);
+}
+
+LightningBolt::LightningBolt(float range, float damage) : range(range), damage(damage)
+{
+}
+
+bool LightningBolt::use(Actor *owner, Actor *wearer)
+{
+    Actor *closestMonster = engine.getClosestMonster(wearer->x, wearer->y, range);
+    if(!closestMonster)
+    {
+        engine.gui->message(TCODColor::lightGrey, "No enemy is close enough to strike.");
+        return false;
+    }
+
+    engine.gui->message(TCODColor::lightBlue, "A lighting bolt strikes the %s with a loud thunder!\n",
+        "The damage is %g hit points.", closestMonster->name, damage);
+    closestMonster->destructible->takeDamage(closestMonster, damage);
+
+    return Pickable::use(owner, wearer);
+}
+
+Fireball::Fireball(float range, float damage) : LightningBolt(range, damage)
+{
+}
+
+bool Fireball::use(Actor *owner, Actor *wearer)
+{
+    engine.gui->message(TCODColor::cyan, "Left-click a target tile for the fireball,\nor right-click to cancel.");
+
+    int x, y;
+    if(!engine.pickATile(&x, &y))
+    {
+        return false;
+    }
+
+    engine.gui->message(TCODColor::orange, "The fireball explodes, burning everything within %g tiles!", range);
+    for(Actor **iterator = engine.actors.begin(); iterator != engine.actors.end(); iterator++)
+    {
+        Actor *actor = *iterator;
+        if(actor->destructible && !actor->destructible->isDead() && actor->getDistance(x, y) <= range)
+        {
+            engine.gui->message(TCODColor::orange, "The %s gets burned for %g hit points.", actor->name, damage);
+            actor->destructible->takeDamage(actor, damage);
+        }
+    }
+
+    return Pickable::use(owner, wearer);
+}
--- Roguelike Tutorial/Pickable.h
+++ Roguelike Tutorial/Pickable.h
@@ -12,4 +12,43 @@ class Pickable
     private:
 };
 
+class Healer : public Pickable
+{
+    public:
+        float amount;
+
+        Healer(float amount);
+        bool use(Actor *owner, Actor *wearer);
+    protected:
+    private:
+};
+
+class Confuser : public Pickable
+{
+    public:
+        int nbTurns;
+        float range;
+
+        Confuser(int nbTurns, float range);
+        bool use(Actor *owner, Actor *wearer);
+};
+
+class LightningBolt : public Pickable
+{
+    public :
+        float range, damage;
+
+        LightningBolt(float range, float damage);
+        bool use(Actor *owner, Actor *wearer);
+};
+
+class Fireball : public LightningBolt
+{
+    public:
+        Fireball(float range, float damage);
+        bool use(Actor *owner, Actor *wearer);
+    protected:
+    private:
+};
+
 #endif // PICKABLE_H
